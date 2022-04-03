627e6f3acd348a5e1363b072a23508a7126b6523
Alireza Naghdipour <alirezanqp@gmail.com>
2022-04-03 12:49:37 +0430

    update

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,11 +1,39 @@
+const { default: mongoose } = require('mongoose')
 const User = require('../models/userModel')
 
 const getUsers = async (req, res, next) => {
-  const users = await User.find()
+  let projection = {}
+  if (req.query.hasOwnProperty('fields')) {
+    projection = req.query.fields.split(',').reduce((total, current) => {
+      return { [current]: 1, ...total }
+    }, {})
+  }
+  const users = await User.find({}, projection)
   res.status(200).json({
     msg: 'لیست کاربران',
     data: users
   })
+}
+
+const getOneUser = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({
+        status: 400,
+        msg: 'id not valid!'
+      })
+    }
+    const user = await User.findOne({ _id: id })
+    if (!user) {
+      return res.status(404).send({ status: 404, msg: 'کاربر مورد نظر یافت نشد!' })
+    }
+    return res.status(200).send({
+      user
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 const addUser = async (req, res, next) => {
@@ -28,7 +56,31 @@ const addUser = async (req, res, next) => {
   }
 }
 
+const deleteUser = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({
+        status: 400,
+        msg: 'id not valid!'
+      })
+    }
+    const user = await User.findByIdAndDelete(id)
+    if (!user) {
+      return res.status(404).send({ status: 404, msg: 'کاربر مورد نظر یافت نشد!' })
+    }
+    return res.status(200).send({
+      msg: 'کاربر موردنظر با موفقیت حذف شد',
+      data: user
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
 module.exports = {
   getUsers,
-  addUser
+  addUser,
+  getOneUser,
+  deleteUser
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getUsers, addUser } = require('../controllers/usersController')
-
+const { getUsers, addUser, getOneUser, deleteUser } = require('../controllers/usersController')
 const { validate, registerValidator } = require('../validator/user')
 
 router.get('/', getUsers)
+router.get('/:id', getOneUser)
+router.delete('/:id', deleteUser)
 router.post('/', registerValidator(), validate, addUser)
 
 module.exports = router
